f55980fd446359aed152d56b6dd6afa2082c70eb
dkap78 <deep.kapasi@smilesoftwareindia.com>
2025-08-04 11:57:54 +0530

    Base Path
    Change the base path in vite.config.js from './' to '/'. As previously site was not loading on mobile devices.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // path are relative
+  base: '/', // path are relative
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
